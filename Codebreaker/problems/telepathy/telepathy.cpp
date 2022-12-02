842016384dc3c80ec11941e9b9d55ad42b989199
fwyr <fwyr@users.noreply.github.com>
2022-12-02 13:23:45 +0800

    Codebreaker: solved telepathy

---
+++ Codebreaker/problems/telepathy/telepathy.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pleasehelp cout << "anyone there?\n";
+using namespace std;
+signed main() {
+    fastio;
+    cout << "0.02";
+    // i think the idea is that
+    // your score is calculated based on
+    // the value you print now
+    // plus the score of the last submission
+    // for some reason i don't think you can 
+    // print a float more than 49.99
+
+    // for example:
+    // if your current score is 0
+    // start with printing 49.99
+    // then 49.99 again to get 99.98
+    // and finally 0.02
+}
