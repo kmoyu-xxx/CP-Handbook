90f12933dee9f7f266a6956ef37b51e25cf72db0
fwyr <fwyr@users.noreply.github.com>
2022-12-02 13:53:05 +0800

    Codebreaker: solved summarising2020

---
+++ Codebreaker/problems/summarising2020/summarising2020.cpp
@@ -0,0 +1,11 @@
+#include "summarising2020.h"
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> message;
+long long xythesnake(int N, vector<int> A) {
+	message = A;
+	return 0;
+}
+vector<int> friendofxy(int N, long long X) {
+	return message;
+}
