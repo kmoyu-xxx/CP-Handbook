cfa4229f5adf32e724a018f2a0035194656c2174
fwyr <fwyr@users.noreply.github.com>
2022-12-06 19:11:02 +0800

    Codebreaker: solved subtaskstitching

---
+++ Codebreaker/problems/subtaskstitching/subtaskstitching.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ping cout << "pong\n";
+using namespace std;
+signed main() {
+    srand(clock());
+    cout << rand() % 2 + 1;
+}
