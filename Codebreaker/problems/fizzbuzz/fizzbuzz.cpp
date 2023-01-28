e4a956b0b50635fa3a5cc3d24f75347fbe82a44e
fwyr <fwyr@users.noreply.github.com>
2023-01-28 16:56:26 +0800

    Codebreaker: update fizzbuzz, rightangtri

--- Codebreaker/problems/fizzbuzz/fizzbuzz.cpp
+++ Codebreaker/problems/fizzbuzz/fizzbuzz.cpp
@@ -1,18 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ping cout << "pong\n";
 using namespace std;
-int main() {
-    int n, a, b;
-    cin >> n >> a >> b;
-
+signed main() {
+    fast;
+    int n, a, b; cin >> n >> a >> b;
     for (int i = 1; i <= n; i++) {
-        if (i % a == 0 && i % b == 0) {
-            cout << "FizzBuzz" << endl;
-        } else if (i % a == 0) {
-            cout << "Fizz" << endl;
-        } else if (i % b == 0) {
-            cout << "Buzz" << endl;
-        } else {
-            cout << i << endl;
-        }
+        if (i % a == 0 && i % b == 0) cout << "FizzBuzz\n";
+        else if (i % a == 0) cout << "Fizz\n";
+        else if (i % b == 0) cout << "Buzz\n";
+        else cout << i << endl;
     }
 }
--- Codebreaker/problems/rightangtri/rightangtri.cpp
+++ Codebreaker/problems/rightangtri/rightangtri.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ping cout << "pong\n";
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-
+signed main() {
+    fast;
+    int n; cin >> n;
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
+        for (int j = 0; j < i; j++) {
             cout << "*";
         }
         cout << endl;
