5829601472c1dda2e9855579ddc4402be2ff6196
fwyr <fwyr@users.noreply.github.com>
2023-04-08 02:09:34 +0800

    Codebreaker: solved goodprogrammer

---
+++ Codebreaker/problems/goodprogrammer/goodprogrammer.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	string s; 
+    getline(cin, s);
+    int x = (s.size() + 1) / 7;
+    for (int i = 0; i < x*3; i++) {
+        cout << "wabbit ";
+    }
+}
+// 2am sgt
