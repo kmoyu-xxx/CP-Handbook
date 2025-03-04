5bccb61e9aec1373189e601067749aab132d23df
fwyr <105589546+fwyr@users.noreply.github.com>
2025-03-05 06:49:51 +0800

    HackerRank: ?? cleanup

--- HackerRank/Python/string-validators.py
+++ HackerRank/Python/string-validators.py
@@ -1,16 +1,10 @@
 if __name__ == '__main__':
-    alnum = False
-    alpha = False
-    digit = False
-    lower = False
-    upper = False
-
     s = input()
-
-    s = [char for char in s]
-
-    for _ in s:
-
-        if _.isalnum() == True:
-            alnum = True
-    
+    alnum = any([char.isalnum() for char in s])
+    alpha = any([char.isalpha() for char in s])
+    digit = any([char.isdigit() for char in s])
+    lower = any([char.islower() for char in s])
+    upper = any([char.isupper() for char in s])
+            
+    print(alnum, alpha, digit, lower, upper, sep='\n')
+        
